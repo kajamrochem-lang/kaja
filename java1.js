dba5fdbd8872ebcec47e904ef999479ac05fbb2d
kajamrochem-lang <kajamrochem@gmail.com>
2025-09-25 10:56:28 +0200

    :(

--- java1.js
+++ java1.js
@@ -46,10 +46,15 @@ console.log(+b)
 console.log(2==2)
 console.log(2!=2)
 
-zadanie4*/
+zadanie4
 let x = 2
 console.log(x)
 console.log(x+=2)
 console.log(x-=2)
 console.log(x*=2)
 console.log(x/=2)
+
+zad5*/
+let x = 1
+console.log("przesuniecie w lewo:"+(x<<1))
+console.log("przesuniecie w prawo:"+(x>>1))
