3481b4f5a4520d03a7d12e5ea7d85755fb8a693b
kajamrochem-lang <kajamrochem@gmail.com>
2025-09-25 09:32:17 +0200

    :(

--- java1.js
+++ java1.js
@@ -1,11 +1,13 @@
-//let naszazmienna = 4 
-//console.log(naszazmienna)
+/*zad1
+let naszazmienna = 4 
+console.log(naszazmienna)
 
-//naszazmienna = 6
+naszazmienna = 6
 
-//console.log(naszazmienna)
-//document.write("wartosci zmiennych mozemy zmieniac a stalych nie")
-//
+console.log(naszazmienna)
+document.write("wartosci zmiennych mozemy zmieniac a stalych nie")
+
+zad2
 let a = 4 //number
 console.log(typeof(a))
 
@@ -23,3 +25,19 @@ console.log(typeof(e))
 
 let f
 console.log(typeof(f))
+
+Zadanie3*/
+let a = 15
+let b = 4
+/*
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+
+Zadanie4*/
+console.log(a%b)
+console.log(a++)
+console.log(a++)
+console.log(b--)
+console.log(b--)
