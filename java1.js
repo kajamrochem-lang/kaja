f07d8753130ff0c7c9446e8f99ef34c5ebca7ddc
kajamrochem-lang <kajamrochem@gmail.com>
2025-09-25 09:17:57 +0200

    :(

---
+++ java1.js
@@ -0,0 +1,25 @@
+//let naszazmienna = 4 
+//console.log(naszazmienna)
+
+//naszazmienna = 6
+
+//console.log(naszazmienna)
+//document.write("wartosci zmiennych mozemy zmieniac a stalych nie")
+//
+let a = 4 //number
+console.log(typeof(a))
+
+let b = "javascript" //string
+console.log(typeof(b))
+
+let c = "javascript" //string
+console.log(typeof(c))
+
+let d = true //bolean
+console.log(typeof(d))
+
+let e = null //null
+console.log(typeof(e))
+
+let f
+console.log(typeof(f))
